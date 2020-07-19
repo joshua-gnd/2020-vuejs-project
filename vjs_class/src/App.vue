59e9cd28c421cddfde544d83144673b408c59076
Joshua Elahie <lexelite36@gmail.com>
2020-07-19 00:48:12 -0400

    first commit

---
+++ vjs_class/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <companies v-for="company in companies" :key="company.name" :company="company"></companies>
+  </div>
+</template>
+
+<script>
+import companies from "./components/companies.vue";
+
+export default {
+  components: {
+    companies
+  },
+
+  data() {
+    return {
+      companies: [
+        {
+          name: "CEEP",
+          address: "St. David, Grenada",
+          phone: "444-5501",
+          email: "ceep@gmail.com"
+        },
+        {
+          name: "SONOVER",
+          address: "St. George, Grenada",
+          phone: "414-1111",
+          email: "snvr@gmail.com"
+        },
+        {
+          name: "FULLSTACK",
+          address: "St. Andrew, Grenada",
+          phone: "423-5512",
+          email: "flstk@gmail.com"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vjs_class/src/components/companies.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="card">
+    <h1>{{company.name}}</h1>
+    <h3>{{company.address}}</h3>
+    <p>{{company.phone}} || {{company.email}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["company"]
+};
+</script>
+
+<style>
+#card {
+  display: block;
+  margin: 1rem auto;
+  border: 2px solid #800020;
+  border-radius: 12px;
+  padding: 0 2rem 1rem;
+  width: 300px;
+  color: #800020;
+  background-color: #ffc0d0;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
